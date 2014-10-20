2635f76de0e3b1620b333e4323b8c1a9eace8258
Primary Lose <Primary@Primarys-MacBook-Pro.local>
2014-10-20 15:04:24 +0700

    test project

---
+++ app/views/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>thaiacinterfresh</title>
+	<script src="http://code.jquery.com/jquery-1.9.0.js"></script>
+	<link rel="stylesheet" href="<?php echo asset('assets/css/layout.css')?>" type="text/css">	
+</head>
+<body>
+	<div id="leftBar" class="leftBar level-0">
+		<!-- <a href="en/#" class="mainNavBack hidden">Back</a> -->
+		<a href="en/#" class="closeBtn"></a>
+		<div id="mainNavContainer" class="mainNavContainer" data-current="1" data-lang="1">
+			<ul id="mainNav" class="nav mainNav level" data-level="1">
+				<li class="page-212"><a href="en.html">Homepage</a></li>
+				<li class="page-18 hasChild"><a>Group</a></li>
+				<li class="page-19 hasChild"><a>Our vegetables</a></li>
+				<li class="page-132 hasChild"><a>Sustainable development</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="mainContent">
+	    <div id="mainContent">
+	      <!--TYPO3SEARCH_begin-->
+	      	<div id="header" class="header">
+		        <div class="page">
+		             <!--Header-->
+		             <h1 class="logo"><a href="#" target="_self"><img src="public/image/THAIAC2_hires.jpg" width="215" height="55" alt="Dummy Generic"></a></h1>
+
+		             <!--topNav-->
+		             <div class="menuTop">
+		                <ul id="topNav" class="topNav"><li class="page-134 ajaxPopIn"><a href="en/in-the-world.html">In the world</a></li><li class="sep"></li><li class="page-135 ajaxPopIn"><a href="en/network.html">Network</a></li><li class="sep"></li><li class="page-16"><a href="en/no_cache/news.html">News</a></li><li class="sep"></li><li class="lang"><a href="fr.html">FR</a></li><li class="sep"></li><li class="search ajaxPopIn page-13"><a href="en/search.html"><img src="typo3conf/ext/skin/Resources/Public/img/bc-ico-recherche-off.jpg" width="13" height="13" class="hover" alt="Search"></a></li></ul>
+
+		                <!-- EN ATTENTE DE MAQUETTAGE -->
+		                <div id="searchTop" class="searchTop">
+						</div>
+		            </div>
+		        </div>
+		    </div>
+			<div id="c3172" class="csc-default">
+				<div class="hometitle">
+					<div class="hometitleContent" style="height: 100%;">
+						<p class="h2-like">Well living<br>
+							<span class="subtitle">through vegetable products</span>
+						</p>
+						<h2 class="link">
+							<a href="en/group/task.html">Our vision</a>
+						</h2>
+						<h2 class="next" style="bottom: 15px; opacity: 1;">
+							<span>Our commitments</span>
+						</h2>
+					</div>
+					<div class="fade">
+						<div class="background" style="z-index: 2; display: none; background-image: url(http://www.bonduelle.com/uploads/skinFlex/hometitle/bc-img1.jpg);"></div>
+						<div class="background" style="z-index: 99; display: block; background-image: url(http://www.bonduelle.com/uploads/skinFlex/hometitle/bc-img3.jpg);"></div>
+					</div>
+				</div>
+			</div>
+			<div id="c3173" class="csc-default">
+				<div class="engagements">
+					<div class="columns desactivateMargins">
+						<div class="block column column-25" style="background-image: url(/uploads/skinFlex/engagements/bc-imgbloc1_01.jpg)">
+							<a href="en/sustainable-development/agricultural-production.html" style="height: auto;">
+								<h3><span class="blockTitle">Respecting</span><span class="blockText">the planet and our social commitments</span><span class="blockLink"><span>More</span></span></h3>
+							</a>
+						</div>
+						<div class="block column column-25" style="background-image: url(/uploads/skinFlex/engagements/bc-imgbloc2_01.jpg)">
+							<a href="en/careers/our-vision.html" style="height: auto;"><h3><span class="blockTitle">Encouraging</span><span class="blockText">the development of our men and women</span><span class="blockLink"><span>More</span></span></h3></a>
+						</div>
+						<div class="block column column-25" style="background-image: url(/uploads/skinFlex/engagements/bc-imgbloc3_01.jpg)">
+							<a href="en/our-vegetables/bonduelle.html" style="height: auto;"><h3><span class="blockTitle">Developing</span><span class="blockText">our brands and vegetables in all their forms</span><span class="blockLink"><span>More</span></span></h3></a>
+						</div>
+						<div class="block column column-25" style="background-image: url(/uploads/skinFlex/engagements/bc-imgbloc4.jpg)">
+							<a href="en/group/organization.html" style="height: auto;"><h3><span class="blockTitle">Ensuring</span><span class="blockText">our organization is customer-oriented</span><span class="blockLink"><span>More</span></span></h3></a>
+						</div>
+					</div>
+				</div>
+			</div><!--TYPO3SEARCH_end-->
+	    </div>
+	</div>	
+	</div>
+	
+</body>
+</html>
